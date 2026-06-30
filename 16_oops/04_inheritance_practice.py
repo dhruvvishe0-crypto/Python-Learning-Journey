e5aa42714f93423e29f0a4f65c477035ba372860
DhruvVishe <dhruvvishe0@gmail.com>
2026-06-30 08:59:37 +0530

    Update 04_inheritance_practice.py

--- 16_oops/04_inheritance_practice.py
+++ 16_oops/04_inheritance_practice.py
@@ -249,3 +249,292 @@ student_1 = student("Dhruv")
 student_1.walk()
 student_1.study()
 
+# Method Overriding is a feature in inheritance where child class can override the parent class method of the same name 
+
+# for example :
+
+class Animal :
+
+    def speak(self):
+        print("Animals make sounds".title())
+
+class Dog(Animal):        # so we use method overriding because dog dont say animals make sounds it says woof woof so we can override it
+    def speak(self):          # here we can see that we have created a method as same name as parent class method
+        print("Woof Woof")
+
+dog_1 = Dog()
+dog_1.speak()      # Python first check in dog class so it founds the speak method in dog class so it dont go to parent class ignore my english
+
+# Most important thing to remember is that if we have same named methods in parent and child class then python goes to child class first
+
+
+
+# Now we will practice some questions on it
+
+"""
+Create a parent class named Vehicle.
+
+Method:
+- start()
+
+Print:
+"Vehicle is starting."
+
+Create a child class named Car.
+
+Override the start() method.
+
+Print:
+"Car is starting."
+
+Create one Car object.
+
+Call start().
+"""
+
+class Vehicle :
+
+    def start(self):
+        print("Vehicle is starting".title())
+
+class car (Vehicle):
+    def start(self):
+        print("car is staring".title())
+
+car_1 = car()
+car_1.start()
+
+
+
+"""
+Create a parent class named Bird.
+
+Method:
+- sound()
+
+Print:
+"Bird makes sound."
+
+Create two child classes:
+
+Parrot
+Crow
+
+Override sound() in both classes.
+
+Parrot should print:
+"Parrot says Hello!"
+
+Crow should print:
+"Crow says Caw Caw!"
+
+Create one object of each class.
+
+Call sound() using both objects.
+"""
+
+
+class Bird :
+    def sound(self):
+        print("birds make sound".title())
+
+class parrot(Bird):
+    def sound(self):
+        print("parrot says hello".title())
+
+class crow(Bird):
+    def sound(self):
+        print("Crow says caw caw".title())
+
+parrot_1 =parrot()
+crow_1 = crow()
+
+parrot_1.sound()
+crow_1.sound()
+
+
+"""
+Create a parent class named GameCharacter.
+
+Method:
+- attack()
+
+Print:
+"Character attacks."
+
+Create three child classes:
+
+Warrior
+Archer
+Wizard
+
+Override attack() in each class.
+
+Warrior:
+"Warrior attacks with a Sword."
+
+Archer:
+"Archer attacks with a Bow."
+
+Wizard:
+"Wizard attacks with Magic."
+
+Create one object of each class.
+
+Call attack() using all three objects.
+"""
+
+class GameCharacter :
+    def attack(self):
+        print("Character attacks".title())
+
+class Warrior(GameCharacter):
+    def attack(self):
+        print("warrior attacks with a sword".title())
+
+class Archer(GameCharacter):
+    def attack(self):
+        print("archer attacks with a bow ".title())
+
+class  Wizard(GameCharacter):
+    def attack(self):
+        print("Wizard attacks with Magic".title())
+
+
+warrior_1 = Warrior()
+archer_1 = Archer()
+wizard_1 = Wizard()
+
+warrior_1.attack()
+archer_1.attack()
+wizard_1.attack()
+
+
+# Now we will learn what is super keyword
+
+class animal:
+    def __init__(self,name):
+        self.name =name
+
+    def eat(self):
+        print(f"{self.name} is eating ".title())
+
+class dog(animal):
+    def __init__(self,name,breed):
+        super().__init__(name)
+        self.breed = breed
+
+
+# we will practice some question on it i mean in super keyword
+'''
+Question 1
+
+Create a parent class named Animal.
+
+Create a constructor that prints "Animal Constructor Called".
+
+Create a child class named Dog.
+
+Use super() to call the parent's constructor.
+Then print "Dog Constructor Called".
+
+Finally, create an object of the Dog class.
+'''
+
+class animal :
+    def __init__(self):
+        print("Animal Constructor called".title())
+
+class dog(animal):
+    def __init__(self):
+        super().__init__()
+        print("dog constructor called".title())
+
+dog_2 = dog()
+
+
+
+
+
+"""
+Create a parent class named Person.
+
+Constructor:
+- name
+- age
+
+Create a child class named Student.
+
+Student should have one extra attribute:
+- college
+
+Use super() to initialize name and age.
+
+Create one object.
+
+Print:
+Name
+Age
+College
+"""
+
+class Person :
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+class student(Person):
+    def __init__(self,name,age,college):
+        super().__init__(name,age)
+        self.college = college
+
+student_1 = student("Dhruv",18,"GPM")
+
+print("name :".title(),student_1.name)
+print("age :".title(),student_1.age)
+print("college :".title(),student_1.college)
+
+
+# there are total 5 types of inheritance :
+
+# 1 ) Single Inheritance :  In single inheritance a parent class have a single child class
+
+
+# 2 ) Multilevel inheritance :in multilevel indexing child class have a  child class means a parent class have grandchild class
+'''
+for example :
+
+class animal:   # this is a parent class
+    pass
+class dog(animal):  # this is a child class
+    pass
+class puppy(dog):  # this is a grandchild class
+    pass
+
+
+# I think that's enough for today
+'''
+
+# 3 ) multiple inheritance : in this a child have 2 parents for example :
+
+class father :
+    pass
+class mother :
+    pass
+
+class child(father,mother):  # this child class has 2 parents
+    pass
+
+# 4 ) Hierarchical inheritance : in this a parent class have multiple children
+
+class animal:
+    pass
+class dog(animal):
+    pass
+class cat(animal):
+    pass
+class cow(animal):
+    pass
+
+# 5 ) hybrid inheritance : it is the combination of two or more types of inheritance
+
